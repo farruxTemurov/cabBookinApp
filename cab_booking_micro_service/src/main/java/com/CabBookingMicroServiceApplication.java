964c537c18d67b1601a2db78007c57429a5bcbbb
Farrux Temurov <tfarrux70@gmail.com>
2025-04-02 22:42:14 +0500

    added cab-booking app

--- cab_booking_micro_service/src/main/java/com/CabBookingMicroServiceApplication.java
+++ cab_booking_micro_service/src/main/java/com/CabBookingMicroServiceApplication.java
@@ -2,12 +2,19 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@EntityScan(basePackages = "com.bean")
+@EnableJpaRepositories(basePackages = "com.repository")
+@EnableDiscoveryClient
 public class CabBookingMicroServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CabBookingMicroServiceApplication.class, args);
+		System.err.println("Booking micro service's up on port 8181");
 	}
 
 }
