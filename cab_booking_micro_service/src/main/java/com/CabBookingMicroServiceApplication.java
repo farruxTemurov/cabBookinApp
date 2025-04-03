325a884bbea23a706ed10ab29b8367a0baf3d588
Farrux Temurov <tfarrux70@gmail.com>
2025-04-03 13:03:20 +0500

    updated service layer

--- cab_booking_micro_service/src/main/java/com/CabBookingMicroServiceApplication.java
+++ cab_booking_micro_service/src/main/java/com/CabBookingMicroServiceApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = "com")
 @EntityScan(basePackages = "com.bean")
@@ -15,6 +18,12 @@ public class CabBookingMicroServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CabBookingMicroServiceApplication.class, args);
 		System.err.println("Booking micro service's up on port 8181");
+	}
+
+	@Bean
+	@LoadBalanced // Enables Eureka to resolve service names automatically
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
--- cab_booking_micro_service/src/main/java/com/service/BookingInfoService.java
+++ cab_booking_micro_service/src/main/java/com/service/BookingInfoService.java
@@ -1,8 +1,21 @@
 package com.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class BookingInfoService {
+	@Autowired
+	RestTemplate restTemplate;
 
+	public float getFare(String source, String destination) {
+		try {
+			String url = "http://FARE-SERVICE/fare/get?source=" + source + "&destination=" + destination;
+			return restTemplate.getForObject(url, Float.class); // Calls the fare service API
+		} catch (Exception e) {
+			System.err.println("Error while fetching fare: " + e.getMessage());
+			return -1; // Return -1 if there's an error
+		}
+	}
 }
